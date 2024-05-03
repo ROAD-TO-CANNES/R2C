a1c483d9d35f219853f9b46fa74f9564809a6704
tom_ballester <137886466+tom123456789123456@users.noreply.github.com>
2024-05-03 18:43:55 +0200

    Fix bug in session verification for enable-disable good practice

--- Acceuil/enableDisableBP.php
+++ Acceuil/enableDisableBP.php
@@ -1,8 +1,24 @@
 <?php
+  session_start(); 
+
+  include '/home/r2c/R2C/bdd.php';
+  
+  // verify if the user is already connected
+  $sql = "SELECT statutcon FROM USER WHERE login LIKE '$_SESSION[name]'";
+  $request = $BDD->prepare($sql);
+  $request->execute();
+  $statutcon = $request->fetchColumn();
+
+  // if the user is not connected, redirect to the login page
+  if($statutcon == 0) {
+    header('Location: ../index.php');
+    exit;
+  }
+
+  include '/home/r2c/R2C/timer.php';
+
   if(isset($_POST['id'])) {
     $idBP = substr($_POST['id'], 6);
-
-    include '../bdd.php';
 
     $sql = "SELECT statut FROM BONNESPRATIQUES WHERE idbp = $idBP";
     $request = $BDD->prepare($sql);
