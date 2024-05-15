36a056f42a335462262b940d713d8f7222a09ba7
tom_ballester <137886466+tom123456789123456@users.noreply.github.com>
2024-05-15 15:31:30 +0200

    fix bug in login form error and message classes

--- Forms/login_form.php
+++ Forms/login_form.php
@@ -5,30 +5,32 @@
 <div class="login-wrap">
   <h2>Connectez vous a <br> R2C</h2>
   <?php
-    if(isset($_GET['error']) && $_GET['error'] == "e1"){
-      echo("
-        <div class=\"error\">
-          <p>Nom d'utilisateur ou mot de passe incorrect</p>
-        </div>"
-      );
-    };
-    if(isset($_GET['error']) && $_GET['error'] == "e2"){
-      echo("
-        <div class=\"error\">
-          <p>Votre compte est bloqué veuillez contacter votre administrateur</p>
-        </div>"
-      );
-    };
-    if(isset($_GET['error']) && $_GET['error'] == "e3"){
-      echo("
-        <div class=\"error\">
-          <p>Désolé, un utilisateur est déjà connecté. Une seule connexion à la fois est autorisée</p>
-        </div>"
-      );
+    if(isset($_GET['error'])){
+      if($_GET['error'] == "e1"){
+        echo("
+          <div class=\"errorlog\">
+            <p>Nom d'utilisateur ou mot de passe incorrect</p>
+          </div>"
+        );
+      };
+      if($_GET['error'] == "e2"){
+        echo("
+          <div class=\"errorlog\">
+            <p>Votre compte est bloqué veuillez contacter votre administrateur</p>
+          </div>"
+        );
+      };
+      if($_GET['error'] == "e3"){
+        echo("
+          <div class=\"errorlog\">
+            <p>Désolé, un utilisateur est déjà connecté. Une seule connexion à la fois est autorisée</p>
+          </div>"
+        );
+      };
     };
     if(isset($_COOKIE['m1']) && $_COOKIE['m1'] == "1") {
       echo("
-        <div class=\"message\">
+        <div class=\"messagelog\">
           <p>Vous avez été déconnecté pour cause d'innactivitée</p>
         </div>"
       );
