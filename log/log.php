1dfd81ffa36686d0f789b321b929833eb7c92cac
bboss204 <93838916+bboss204@users.noreply.github.com>
2024-05-20 23:34:50 +0200

    chore: Update log.php and log.css for logs page

--- log/log.php
+++ log/log.php
@@ -2,7 +2,7 @@
   session_start(); 
   include '/var/www/r2c.uca-project.com/Forms/checkSession.php';
 
-  if (!$_SESSION['droits'] > 0) {
+  if (!$_SESSION['droits'] > 1) {
     header('Location: ../Accueil/accueil.php');
   }
 ?>
@@ -10,8 +10,8 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>R2C - Nouveau programme</title>
-    <link rel="stylesheet" type="text/css" href="./newProg.css">
+    <title>R2C - logs</title>
+    <link rel="stylesheet" type="text/css" href="./log.css">
     <link rel="icon" type="image/png" href="../Img/icon.png">
   </head>
   <header>
@@ -19,32 +19,8 @@
   </header>
   <body>
     <div class="top">
-      <h1>Créer un nouveau programme</h1>
+      <h1>consulter les logs</h1>
     </div>
-    <form
-      id="newProg-form"
-      class="newProg-form"
-      action="../NewProg/newProgScript.php"
-      method="post"
-    >
-      <input
-        type="text"
-        id="nomprog"
-        name="nomprog"
-        placeholder="Nom du programme"
-        required
-      />
-      <textarea
-        id="descprog"
-        name="descprog"
-        placeholder="Décrivez le programme"
-        rows="4" 
-        cols="50"
-        required
-      ></textarea>
-      <button class="enregistrer" type="submit" value="Enregistrer">Enregistrer</button>
-      <a href="../Accueil/accueil.php"><button class="annulerProg" type="button">Annuler</button></a>
-    </form>
   </body>
   <script src="../timer.js"></script>
 </html>
