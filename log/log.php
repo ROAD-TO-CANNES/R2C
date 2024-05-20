0a5ccf8aeb6a899bbec343e1cc29f8b1a63808dc
bboss204 <93838916+bboss204@users.noreply.github.com>
2024-05-21 00:01:54 +0200

    chore: Update log.php and log.css for logs page

--- log/log.php
+++ log/log.php
@@ -21,6 +21,30 @@
     <div class="top">
       <h1>Consulter les logs</h1>
     </div>
+    <div class="content">
+      <table>
+        <tr>
+          <th>Date</th>
+          <th>Utilisateur</th>
+          <th>Page</th>
+          <th>Action</th>
+        </tr>
+        <?php
+          $sql = "SELECT * FROM LOGS";
+          $request = $BDD->prepare($sql);
+          $request->execute();
+          $logs = $request->fetchAll();
+
+          foreach($logs as $log) {
+            echo '<tr>';
+            echo '<td>' . $log['datea'] . '</td>';
+            echo '<td>' . $log['login'] . '</td>';
+            echo '<td>' . $log['desca'] . '</td>';
+            echo '</tr>';
+          }
+        ?>
+      </table>
+    </div>
   </body>
   <script src="../timer.js"></script>
 </html>
