c757b93a24736fae3fb9de958ed2703f74aaa460
bboss204 <93838916+bboss204@users.noreply.github.com>
2024-05-20 23:32:12 +0200

    chore: Update log.css and log.php for new program creation form

--- log/log.php
+++ log/log.php
@@ -1,27 +1,50 @@
-<?php
+<?php 
   session_start(); 
-
   include '/var/www/r2c.uca-project.com/Forms/checkSession.php';
 
-  //Récuperation des utilisateurs
-  $sql = "SELECT * FROM USER";
-  $request = $BDD->prepare($sql);
-  $request->execute();
-  $users = $request->fetchAll();
+  if (!$_SESSION['droits'] > 0) {
+    header('Location: ../Accueil/accueil.php');
+  }
 ?>
+
 <!DOCTYPE html>
 <html>
   <head>
-    <title>R2C - Admin</title>
-    <link rel="stylesheet" type="text/css" href="../log/log.css">
+    <title>R2C - Nouveau programme</title>
+    <link rel="stylesheet" type="text/css" href="./newProg.css">
     <link rel="icon" type="image/png" href="../Img/icon.png">
   </head>
+  <header>
+    <?php include '/var/www/r2c.uca-project.com/Header/header.php'; ?>
+  </header>
   <body>
-    <header>
-        <?php include '/var/www/r2c.uca-project.com/Header/header.php'; ?>
-    </header>
     <div class="top">
-      <h1>consultez les logs de l'application</h1>
+      <h1>Créer un nouveau programme</h1>
     </div>
+    <form
+      id="newProg-form"
+      class="newProg-form"
+      action="../NewProg/newProgScript.php"
+      method="post"
+    >
+      <input
+        type="text"
+        id="nomprog"
+        name="nomprog"
+        placeholder="Nom du programme"
+        required
+      />
+      <textarea
+        id="descprog"
+        name="descprog"
+        placeholder="Décrivez le programme"
+        rows="4" 
+        cols="50"
+        required
+      ></textarea>
+      <button class="enregistrer" type="submit" value="Enregistrer">Enregistrer</button>
+      <a href="../Accueil/accueil.php"><button class="annulerProg" type="button">Annuler</button></a>
+    </form>
   </body>
+  <script src="../timer.js"></script>
 </html>
