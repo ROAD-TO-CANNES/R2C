480aa40fdbb2ce3ae04f7f8e04ac9b2891902511
bboss204 <93838916+bboss204@users.noreply.github.com>
2024-05-26 14:30:20 +0200

    chore: Refactor log page filter functionality and improve user experience

--- log/log.php
+++ log/log.php
@@ -19,23 +19,24 @@
   <body>
     <div class="top">
       <h1>Consulter les logs</h1>
-      <!-- <button id="filtre"><img src="../Img/filter.png" alt="filtrer"></button> -->
-      <form action="NULL" method="GET" >
-        <input type="date" name="filtre" id="filtreInput" placeholder="Filtrer par utilisateur">
-        <select>
-          <option value="NULL">filter by user</option>
-          <option value="1">admin</option>
-          <option value="2">tom</option>
-          <option value="3">hugo</option>
-          <option value="4">test</option>
+      <form action="" method="GET" >
+        <input type="date" name="date" id="filtreInput" value="<?= isset($_GET['date'])==true ? $_GET['date']:'' ?>" placeholder="Filtrer par utilisateur">
+        <select name="user">
+          <option value="">filter by user</option>
+          <option value="admin" <?= isset($_GET['user'])==true ? ($_GET['user']=='admin' ? 'selected':'' ):'' ?> >admin</option>
+          <option value="tom" <?= isset($_GET['user'])==true ? ($_GET['user']=='tom' ? 'selected':'' ):'' ?> >tom</option>
+          <option value="hugo" <?= isset($_GET['user'])==true ? ($_GET['user']=='hugo' ? 'selected':'' ):'' ?> >hugo</option>
+          <option value="elouan" <?= isset($_GET['user'])==true ? ($_GET['user']=='elouan' ? 'selected':'' ):'' ?> >elouan</option>
+          <option value="tarik" <?= isset($_GET['user'])==true ? ($_GET['user']=='tarik' ? 'selected':'' ):'' ?> >tarik</option>
+          <option value="test" <?= isset($_GET['user'])==true ? ($_GET['user']=='test' ? 'selected':'' ):'' ?> >test</option>
         </select>
-        <select>
-          <option value="NULL">filter by action</option>
-          <option value="1">Connexion réussie</option>
-          <option value="2">Déconnexion automatique après 5 minutes d'inactivité</option>
-          <option value="3">Déconnexion manuelle réussie</option>
-          <option value="4">Connexion échouée l'utilisateur "admin" est déjà connecté</option>
-          <option value="5">Connexion échouée mot de passe incorrect</option>
+        <select name="action">
+          <option value="">filter by action</option>
+          <option value="1"  <?= isset($_GET['action'])==true ? ($_GET['action']=='1' ? 'selected':'' ):'' ?> >Connexion réussie</option>
+          <option value="2"  <?= isset($_GET['action'])==true ? ($_GET['action']=='2' ? 'selected':'' ):'' ?> >Déconnexion automatique après 5 minutes d'inactivité</option>
+          <option value="3"  <?= isset($_GET['action'])==true ? ($_GET['action']=='3' ? 'selected':'' ):'' ?> >Déconnexion manuelle réussie</option>
+          <option value="4"  <?= isset($_GET['action'])==true ? ($_GET['action']=='4' ? 'selected':'' ):'' ?> >Connexion échouée l'utilisateur "admin" est déjà connecté</option>
+          <option value="5"  <?= isset($_GET['action'])==true ? ($_GET['action']=='5' ? 'selected':'' ):'' ?> >Connexion échouée mot de passe incorrect</option>
         </select>
         <button type="submit" id="filtre"><img src="../Img/filter.png" alt="filtrer"><a href="../log/log.php"></a></button>
       </form>
