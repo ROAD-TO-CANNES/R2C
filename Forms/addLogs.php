3a77c976b1153badea9ce01ed092a1aca101e2e3
tom_ballester <137886466+tom123456789123456@users.noreply.github.com>
2024-05-28 23:18:41 +0200

    Change timezone

--- Forms/addLogs.php
+++ Forms/addLogs.php
@@ -3,8 +3,9 @@
     $encote_typelog = $BDD->quote($typelog);
     $encote_desclog = $BDD->quote($desclog);
     $encote_loginlog = $BDD->quote($loginlog);
+    date_default_timezone_set('Europe/Paris');
     $datelog = date('Y-m-d H:i:s');
-
+    
     $sql = "INSERT INTO LOGS (type, datea, desca, login) VALUES ($encote_typelog, '$datelog', $encote_desclog, $encote_loginlog)";
     $request = $BDD->prepare($sql);
     $request->execute();
