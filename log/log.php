477cebea3342c1b4b0b291913a897a53e2238dcc
tom_ballester <137886466+tom123456789123456@users.noreply.github.com>
2024-05-31 17:43:24 +0200

    Update logs style

--- log/log.php
+++ log/log.php
@@ -5,6 +5,12 @@
   if (!$_SESSION['droits'] > 1) {
     header('Location: ../Accueil/accueil.php');
   }
+
+  //recuperation des utilisateurs
+  $sql = "SELECT login FROM USER";
+  $request = $BDD->prepare($sql);
+  $request->execute();
+  $users = $request->fetchAll();
 ?>
 <!DOCTYPE html>
 <html>
@@ -23,20 +29,17 @@
         <input type="date" name="date" id="filtreInput" value="<?= isset($_GET['date'])==true ? $_GET['date']:'' ?>" placeholder="Filtrer par utilisateur">
         <select name="user">
           <option value="">filter by user</option>
-          <option value="admin" <?= isset($_GET['user'])==true ? ($_GET['user']=='admin' ? 'selected':'' ):'' ?> >admin</option>
-          <option value="tom" <?= isset($_GET['user'])==true ? ($_GET['user']=='tom' ? 'selected':'' ):'' ?> >tom</option>
-          <option value="hugo" <?= isset($_GET['user'])==true ? ($_GET['user']=='hugo' ? 'selected':'' ):'' ?> >hugo</option>
-          <option value="elouan" <?= isset($_GET['user'])==true ? ($_GET['user']=='elouan' ? 'selected':'' ):'' ?> >elouan</option>
-          <option value="tarik" <?= isset($_GET['user'])==true ? ($_GET['user']=='tarik' ? 'selected':'' ):'' ?> >tarik</option>
-          <option value="test" <?= isset($_GET['user'])==true ? ($_GET['user']=='test' ? 'selected':'' ):'' ?> >test</option>
+          <?php
+            foreach ($users as $user) {
+              echo '<option value="'.$user['login'].'" '.(isset($_GET['user'])==true ? ($_GET['user']==$user['login'] ? 'selected':'' ):'' ).' >'.$user['login'].'</option>';
+            }
+          ?> 
         </select>
         <select name="action">
-          <option value="">filter by action</option>
-          <option value="Connexion réussie"  <?= isset($_GET['action'])==true ? ($_GET['action']=='Connexion réussie' ? 'selected':'' ):'' ?> >Connexion réussie</option>
-          <option value="Déconnexion automatique après 5 minutes d\'inactivité"  <?= isset($_GET['action'])==true ? ($_GET['action']=='Déconnexion automatique après 5 minutes d\'inactivité' ? 'selected':'' ):'' ?> >Déconnexion automatique après 5 minutes d'inactivité</option>
-          <option value="Déconnexion manuelle réussie"  <?= isset($_GET['action'])==true ? ($_GET['action']=='Déconnexion manuelle réussie' ? 'selected':'' ):'' ?> >Déconnexion manuelle réussie</option>
-          <option value="Connexion échouée l\'utilisateur admin est déjà connecté"  <?= isset($_GET['action'])==true ? ($_GET['action']=='Connexion échouée l\'utilisateur admin est déjà connecté' ? 'selected':'' ):'' ?> >Connexion échouée l'utilisateur admin est déjà connecté</option>
-          <option value="Connexion échouée mot de passe incorrect"  <?= isset($_GET['action'])==true ? ($_GET['action']=='Connexion échouée mot de passe incorrect' ? 'selected':'' ):'' ?> >Connexion échouée mot de passe incorrect</option>
+          <option value="">filter by type</option>
+          <option value="Information"  <?= isset($_GET['action'])==true ? ($_GET['action']=='Information' ? 'selected':'' ):'' ?> >Information</option>
+          <option value="Warning"  <?= isset($_GET['action'])==true ? ($_GET['action']=='Warning' ? 'selected':'' ):'' ?> >Warning</option>
+          <option value="Alert"  <?= isset($_GET['action'])==true ? ($_GET['action']=='Alert' ? 'selected':'' ):'' ?> >Alert</option>
         </select>
         <button type="submit" id="filtre"><img src="../Img/filter.png" alt="filtrer"><a href="../log/log.php"></a></button>
       </form>
@@ -45,9 +48,10 @@
       <table class="content-table">
         <thead>
           <tr>
-            <th>Date</th>
-            <th>Utilisateur</th>
-            <th>Action</th>
+          <th>Date</th>
+          <th>Type</th>
+          <th>Description</th>
+          <th>Utilisateur</th>
           </tr>
         </thead>
         <tbody>
@@ -56,28 +60,53 @@
             $datea = '%' . $_GET['date'] . '%';
             $login = '%' . $_GET['user'] . '%';
             $type = '%' . $_GET['action'] . '%';
-            $sql = "SELECT * FROM LOGS WHERE datea LIKE '$datea' AND login LIKE '$login' AND desca LIKE '$type' ORDER BY datea DESC LIMIT 10;";
+            $sql = "SELECT * FROM LOGS WHERE datea LIKE '$datea' AND login LIKE '$login' AND desca LIKE '$type' ORDER BY datea DESC;";
             $request = $BDD->prepare($sql);
             $request->execute();
             $logs = $request->fetchAll();
           } else {
-            $sql = "SELECT * FROM LOGS WHERE idlog > 0 ORDER BY datea DESC LIMIT 20;";
+            $sql = "SELECT * FROM LOGS WHERE idlog > 0 ORDER BY datea DESC;";
             $request = $BDD->prepare($sql);
             $request->execute();
             $logs = $request->fetchAll();
           }
 
+          if (isset($_GET['page'])) {
+            $page = $_GET['page'];
+          } else {
+            $page = 1;
+          }
+
+          $start = ($page - 1) * 20;
+          $end = $start + 20;
+          $logs = array_slice($logs, $start, $end);
+
           foreach ($logs as $log) {
+            $date = date('d/m/Y \à H\hi, s\s', strtotime($log['datea']));
+
+            if ($log['type'] == 'Information') {
+              $type = '<td>Information</td>';
+            } elseif ($log['type'] == 'Warning') {
+              $type = '<td style="color: orange;">Warning</td>';
+            } elseif ($log['type'] == 'Alert') {
+              $type = '<td style="color: red;">Alert</td>';
+            }
+
             echo '<tr>';
-            echo '<td>' . $log['datea'] . '</td>';
-            echo '<td>' . $log['login'] . '</td>';
-            echo '<td>' . $log['desca'] . '</td>';
+              echo '<td>' . $date . '</td>';
+              echo $type;
+              echo '<td>' . $log['desca'] . '</td>';
+              echo '<td>' . $log['login'] . '</td>';
             echo '</tr>';
           }
           ?>
         </tbody>
       </table>
     </div>
+    <p>20 résultats affichés</p>
+    <a href="?page=<?= $page - 1 ?>">Page précédente</a>
+    <a href="?page=<?= $page + 1 ?>">Page suivante</a>
+    <?= $start.'-'.$end ?>
   </body>
   <script src="../timer.js"></script>
 </html>
