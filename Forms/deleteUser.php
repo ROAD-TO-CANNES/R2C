b8a4512eb12393e51c0b2cc61d0aedf5cabb18df
tom_ballester <137886466+tom123456789123456@users.noreply.github.com>
2024-06-08 19:14:40 +0200

    add logs for user deletion

--- Forms/deleteUser.php
+++ Forms/deleteUser.php
@@ -7,8 +7,19 @@
     $sql = "DELETE FROM USER WHERE login = ?";
     $request = $BDD->prepare($sql);
     $request->execute([$login]);
+
+    // Logs de suppression d'utilisateur
+    $typelog = "Information";
+    $desclog = "Utilisateur '".$login."' supprimé avec succés";
+    $loginlog = $_SESSION['name'];
+    include '/var/www/r2c.uca-project.com/Forms/addLogs.php';
     header('Location: ../Validation/validation.php?message=suppruser');
   } else {
+    // Logs d'erreur lors de la suppréssion d'utilisateur
+    $typelog = "Warning";
+    $desclog = "Erreur lors de la suppression d'un utilisateur certains paramètres sont manquant";
+    $loginlog = $_SESSION['name'];
+    include '/var/www/r2c.uca-project.com/Forms/addLogs.php';
     header('Location: ../Validation/validation.php?message=ersuppruser');
   }
 ?>
