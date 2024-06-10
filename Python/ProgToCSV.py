b0c3f019d2ee22873dc060465aa0ffd6e0df049b
tom_ballester <137886466+tom123456789123456@users.noreply.github.com>
2024-06-10 15:01:29 +0200

    update python

--- Python/ProgToCSV.py
+++ Python/ProgToCSV.py
@@ -2,7 +2,7 @@ import mariadb
 import csv
 import sys
 
-def generate_csv(idbp_list):
+def generate_csv(idbp_list, mots_clefs_list, phase_list):
     # Se connecter à la base de données
     db = mariadb.connect(
         host="localhost",
@@ -16,9 +16,16 @@ def generate_csv(idbp_list):
     cursor = db.cursor()
 
     # Créer un fichier CSV
-    with open('/var/www/r2c.uca-project.com/Python/Download/Bonnes_Pratiques.csv', 'w', newline='') as file:
+    with open('/var/www/r2c.uca-project.com/Python/Download/Bonnes_Pratiques.csv', 'w', newline='', encoding='utf-8-sig') as file:
+        
         writer = csv.writer(file, delimiter=';')
+        writer.writerow(["Phase", "Mots clés"])
         # Ajouter les en-têtes du tableau
+        for phase in phase_list:
+            mots_clefs = ', '.join(mots_clefs_list)
+            writer.writerow([phase, mots_clefs])
+
+        writer.writerow(["", ""])
         writer.writerow(["Nom", "Description"])
 
         for idbp in idbp_list:
@@ -32,14 +39,20 @@ def generate_csv(idbp_list):
                 nombp = str(row[0])
                 descbp = str(row[1])
                 writer.writerow([nombp, descbp])
-
+        writer.writerow(["Fait", "\u2610"])
     # Fermer la connexion à la base de données
     cursor.close()
     db.close()
 
 if __name__ == "__main__":
     # Convertir les arguments de la ligne de commande en entiers
-    idbp_list = [int(idbp) for idbp in sys.argv[1:]]
+    idbp_list = [int(idbp) for idbp in sys.argv[1].split()]
 
-    # Appeler la fonction avec la liste d'idbp
-    generate_csv(idbp_list)
+    # Get the list of mots_clefs
+    mots_clefs_list = sys.argv[2].split()
+
+    # Get the list of phases
+    phase_list = sys.argv[3].split()
+
+    # Call the function with the idbp list
+    generate_csv(idbp_list, mots_clefs_list, phase_list)
--- Python/ProgToPDF.py
+++ Python/ProgToPDF.py
@@ -1,12 +1,18 @@
 
 import mariadb
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.lib.pagesizes import letter
+from reportlab.platypus import Spacer
 from reportlab.lib import colors
+from reportlab.lib.units import inch
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.platypus import Paragraph
 import sys
 # ...
 
-def generate_pdf(idbp_list):
+def generate_pdf(idbp_list, mots_clefs_list, phase_list):
     # Connect to the database
     db = mariadb.connect(
         host="localhost",
@@ -20,9 +26,20 @@ def generate_pdf(idbp_list):
     cursor = db.cursor()
     # Create a SimpleDocTemplate for the pdf
     doc = SimpleDocTemplate("/var/www/r2c.uca-project.com/Python/Download/Bonnes_Pratiques.pdf", pagesize=letter)
+    pdfmetrics.registerFont(TTFont('DejaVuSans', 'DejaVuSans.ttf'))
+    # Get the default style
+    styles = getSampleStyleSheet()
+
+    # Create the bold style
+    bold_style = styles['Heading2']
+    data1 = [[Paragraph("<b>Phase</b>", bold_style), Paragraph("<b>Mots Clés</b>", bold_style)]]
+    for phase in phase_list:
+        mots_clefs = ', '.join(mots_clefs_list)
+        data1.append([phase, mots_clefs])
 
     # Prepare data for the table
-    data = [["Nom", "Description"]]
+    bold_style = styles['Heading2']
+    data2 = [[Paragraph("<b>Nom</b>", bold_style), Paragraph("<b>Description</b>", bold_style)]]
     for idbp in idbp_list:
         # Execute the query to retrieve the text
         query = f"SELECT nombp, descbp, idbp FROM BONNESPRATIQUES WHERE idbp = {idbp};"
@@ -33,20 +50,29 @@ def generate_pdf(idbp_list):
         for row in result:
             nombp = str(row[0])
             descbp = str(row[1])
-            data.append([nombp, descbp])
+            data2.append([nombp, descbp])
 
     # Calculate the width of each column
     page_width = doc.pagesize[0] * 0.9
     colWidth_id = page_width / 9
     colWidth_desc = 8 * page_width / 9
 
-    # Create a Table with the data and add it to the elements to be added to the pdf
-    table = Table(data, colWidths=[colWidth_id, colWidth_desc])
-    table.setStyle(TableStyle([('GRID', (0,0), (-1,-1), 1, colors.black)]))  # Add grid to table
+    table1 = Table(data1, colWidths=[colWidth_id, colWidth_desc])
+    table1.setStyle(TableStyle([('GRID', (0,0), (-1,-1), 1, colors.black),
+        ('FONT', (0, 0), (-1, -1), 'DejaVuSans')]))  # Add grid to table
+    table1.setStyle(TableStyle([('BACKGROUND', (0, 0), (-1, 0), colors.lightgrey)]))  # Add background color to header
 
+    bold_style = styles['Heading3']
+    data2.append([Paragraph("<b>Fait</b>", bold_style), "\u2610"])
+    table2 = Table(data2, colWidths=[colWidth_id, colWidth_desc])
+    table2.setStyle(TableStyle([('GRID', (0,0), (-1,-1), 1, colors.black),
+        ('FONT', (0, 0), (-1, -1), 'DejaVuSans')]))  # Add grid to table
+    table2.setStyle(TableStyle([('BACKGROUND', (0, 0), (-1, 0), colors.lightgrey)]))
     # Add the table to the elements to be added to the pdf
     elements = []
-    elements.append(table)
+    elements.append(table1)
+    elements.append(Spacer(1, 0.2 * inch))  # Add some space between the tables
+    elements.append(table2)
 
     # Build the pdf
     doc.build(elements)
@@ -56,8 +82,10 @@ def generate_pdf(idbp_list):
     db.close()
 
 if __name__ == "__main__":
-    # Convert the command line arguments to integers
-    idbp_list = [int(idbp) for idbp in sys.argv[1:]]
+    # Convert the command line arguments to lists
+    idbp_list = [int(idbp) for idbp in sys.argv[1].split() if idbp.isdigit()]
+    mots_clefs_list = sys.argv[2].split()
+    phase_list = sys.argv[3].split()
 
-    # Call the function with the idbp list
-    generate_pdf(idbp_list)
+    # Call the function with the idbp list, mots_clefs list, and phase list
+    generate_pdf(idbp_list, mots_clefs_list, phase_list)
