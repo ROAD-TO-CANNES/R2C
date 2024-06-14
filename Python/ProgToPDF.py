adccf044be79277900a83bd895fb77df6676b487
tom_ballester <137886466+tom123456789123456@users.noreply.github.com>
2024-06-14 09:50:53 +0200

    Fix bug in python for PDF

--- Python/ProgToPDF.py
+++ Python/ProgToPDF.py
@@ -2,18 +2,17 @@
 import mariadb
 import argparse
 from datetime import datetime
-import shlex
+import re
+from reportlab.lib.enums import TA_CENTER
+from reportlab.lib.pagesizes import A3
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
-from reportlab.lib.pagesizes import letter
 from reportlab.platypus import Spacer
 from reportlab.lib import colors
 from reportlab.lib.units import inch
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.platypus import Paragraph
-import sys
-import re
 # Function to insert line breaks at appropriate places
 def insert_line_breaks(text, max_length):
     words = text.split(' ')
@@ -42,11 +41,18 @@ def generate_pdf(idbp_list, prog_list, phase_list, keyword_list, user):
         database="project",
         port=8457
     )
-
     # Create a cursor to execute queries
     cursor = db.cursor()
     # Create a SimpleDocTemplate for the pdf
-    doc = SimpleDocTemplate("/var/www/r2c.uca-project.com/Python/Download/Bonnes_Pratiques.pdf", pagesize=letter)
+    doc = SimpleDocTemplate(
+        "/var/www/r2c.uca-project.com/Python/Download/Bonnes_Pratiques.pdf",
+        pagesize=A3,  # Use A3 size instead of A4
+        rightMargin=72,
+        leftMargin=72,
+        topMargin=72,
+        bottomMargin=18
+    )    
+
     pdfmetrics.registerFont(TTFont('DejaVuSans', 'DejaVuSans.ttf'))
     # Get the default style
     styles = getSampleStyleSheet()
@@ -69,7 +75,6 @@ def generate_pdf(idbp_list, prog_list, phase_list, keyword_list, user):
         phase = cursor.fetchone()
         if phase is not None:
             phases.append(phase[0])
-
     # Retrieve all the programs
     progs = []
     for id_prog in prog_list:
@@ -82,8 +87,14 @@ def generate_pdf(idbp_list, prog_list, phase_list, keyword_list, user):
     phase_p = Paragraph(', '.join(phases) if phases else "", styles['BodyText'])
     prog_p = Paragraph(', '.join(progs) if progs else "", styles['BodyText'])
     keyword_p = Paragraph(', '.join(keywords) if keywords else "", styles['BodyText'])
-
-    data1.append([phase_p, prog_p, keyword_p])  # Add data for the third column
+     # Calculate the width of each column
+    page_width = doc.pagesize[0] * 0.9
+    # Check if all lists are empty
+    if not phases and not progs and not keywords:
+        # Add an empty row
+        data1.append([Paragraph("", styles['BodyText']), Paragraph("", styles['BodyText']), Paragraph("", styles['BodyText'])])
+    else:
+        data1.append([phase_p, prog_p, keyword_p])  # Add data for the third column
 
     # Prepare data for the table
     bold_style = styles['Heading2']
@@ -96,30 +107,49 @@ def generate_pdf(idbp_list, prog_list, phase_list, keyword_list, user):
 
         # Add the retrieved data to the table
         for row in result:
-            nombp = insert_line_breaks(str(row[0]), 50)
-            descbp = insert_line_breaks(str(row[1]), 50)
+            nombp = insert_line_breaks(str(row[0]), 105)
+            descbp = insert_line_breaks(str(row[1]), 105)
             nombp_p = Paragraph(nombp, styles['BodyText'])
             descbp_p = Paragraph(descbp, styles['BodyText'])
-            data2.append([nombp_p, descbp_p, "\u2610"])
+            data2.append([nombp_p, descbp_p, "\u2610"])            
+            
+    colWidth2_1 = page_width * 0.25  # First column is smaller
+    colWidth2_2 = page_width * 0.65   # Second column is larger
+    colWidth2_3 = page_width * 0.1  # Third column is smaller
 
-    # Calculate the width of each column
-    page_width = doc.pagesize[0] * 0.9
-    colWidth = page_width / 3  # All columns have the same width
-    colWidth_empty = page_width / 10  # Empty column that we will use later
+    colWidth2_12 = page_width * 0.375  
+    colWidth2_13 = page_width * 0.375 
+    # Adjust the font size
+    styles = getSampleStyleSheet()
+    styles['BodyText'].fontSize = 8
+    styles['Heading3'].fontSize = 10
 
-    table1 = Table(data1, colWidths=[colWidth, colWidth, colWidth])
-    table1.setStyle(TableStyle([('GRID', (0,0), (-1,-1), 1, colors.black),
-        ('FONT', (0, 0), (-1, -1), 'DejaVuSans')]))  # Add grid to table
-    table1.setStyle(TableStyle([('BACKGROUND', (0, 0), (-1, 0), colors.lightgrey)]))  # Add background color to header
+    # Adjust the padding for the tables
+    table1 = Table(data1, colWidths=[colWidth2_1, colWidth2_12, colWidth2_13])
+    table1.setStyle(TableStyle([
+    ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
+    ('FONT', (0, 0), (-1, -1), 'DejaVuSans'),
+    ('BACKGROUND', (0, 0), (-1, 0), colors.lightgrey),
+    ('BOX', (0, 0), (-1, -1), 0.25, colors.black),
+    ('VALIGN', (0, 0), (-1, -1), 'TOP'),
+    ('LEFTPADDING', (0, 0), (-1, -1), 3),  # Adjust the left padding
+    ('RIGHTPADDING', (0, 0), (-1, -1), 3),  # Adjust the right padding
+    ('TOPPADDING', (0, 0), (-1, -1), 3),  # Adjust the top padding
+    ('BOTTOMPADDING', (0, 0), (-1, -1), 3),  # Adjust the bottom padding
+    ]))
 
-    colWidth_id = page_width / 4  # First column is smaller
-    colWidth_desc = 3 * page_width / 6  # Second column is larger
-    bold_style = styles['Heading3']
-    data2.append([Paragraph("<b>Fait</b>", bold_style), "\u2610"])
-    table2 = Table(data2, colWidths=[colWidth_id, colWidth_desc, colWidth_empty])
-    table2.setStyle(TableStyle([('GRID', (0,0), (-1,-1), 1, colors.black),
-        ('FONT', (0, 0), (-1, -1), 'DejaVuSans')]))  # Add grid to table
-    table2.setStyle(TableStyle([('BACKGROUND', (0, 0), (-1, 0), colors.lightgrey)]))
+    table2 = Table(data2, colWidths=[colWidth2_1, colWidth2_2, colWidth2_3])
+    table2.setStyle(TableStyle([
+    ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
+    ('FONT', (0, 0), (-1, -1), 'DejaVuSans'),
+    ('BACKGROUND', (0, 0), (-1, 0), colors.lightgrey),
+    ('BOX', (0, 0), (-1, -1), 0.25, colors.black),
+    ('VALIGN', (0, 0), (-1, -1), 'TOP'),
+    ('LEFTPADDING', (0, 0), (-1, -1), 3),  # Adjust the left padding
+    ('RIGHTPADDING', (0, 0), (-1, -1), 3),  # Adjust the right padding
+    ('TOPPADDING', (0, 0), (-1, -1), 3),  # Adjust the top padding
+    ('BOTTOMPADDING', (0, 0), (-1, -1), 3),  # Adjust the bottom padding
+    ]))
     # Add the table to the elements to be added to the pdf
     elements = []
     elements.append(table1)
@@ -130,15 +160,15 @@ def generate_pdf(idbp_list, prog_list, phase_list, keyword_list, user):
     current_date = datetime.now().strftime("%d/%m/%Y à %H:%M:%S")
 
     # Create the comment
-    comment = f"<para leftIndent=120><i>Ce document a été créé le {current_date}</i></para>"
-    comment_p = Paragraph(comment, styles['BodyText'])
+    styles.add(ParagraphStyle(name='Bigger', parent=styles['BodyText'],alignment=TA_CENTER, spaceAfter=12, fontSize=18))
+    comment = f"<para><i>Ce document a été créé le {current_date}</i></para>"
+    comment_p = Paragraph(comment, styles['Bigger'])
 
     # Add the comment to the elements to be added to the pdf
     elements.append(Spacer(1, 0.3 * inch))  # Add some space before the comment
     elements.append(comment_p)
-    user_comment = f"<para leftIndent=210><i>Par {user}</i></para>"
-    user_comment_p = Paragraph(user_comment, styles['BodyText'])
-
+    user_comment = f"<para><i>Par {user}</i></para>"
+    user_comment_p = Paragraph(user_comment, styles['Bigger'])  
     # Add the user comment to the elements to be added to the pdf
     elements.append(user_comment_p)
 
